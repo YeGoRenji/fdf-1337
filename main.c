a184983a7fdb30068cfb765dbc93b814b7e833a6
Youssef Lyoussfi <yego.designs@gmail.com>
2023-04-17 05:37:08 +0000

    Feat: Kinda AA ? Changed: Cleaned Draw_line code

--- main.c
+++ main.c
@@ -17,39 +17,60 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, uint32_t color)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
-
+	if (!(0 <= x && x < WINDOW_WIDTH))
+		return;
+	if (!(0 <= y && y < WINDOW_HEIGHT))
+		return;
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
+void	swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+# define AA 1
+
 void	draw_line(t_data* img_ptr, int x1, int y1, int x2, int y2)
 {
-	// int	dx = x2 - x1;
-	// int	dy = y2 - y1;
-	int	y_put = y1;
+	int y_x_flip = 0;
 	int	dir_y = (y2 - y1 > 0) ? 1 : -1;
 	int	dir_x = (x2 - x1 > 0) ? 1 : -1;
 
 	// This is where a line shall be born !
+	if (ABS(y2 - y1) >= ABS(x2 - x1))
+	{
+		y_x_flip = 1;
+		swap_int(&x1, &y1);
+		swap_int(&x2, &y2);
+		swap_int(&dir_x, &dir_y);
+	}
+
+	int	y_put = y1;
 	for (int x = x1; x * dir_x <= x2 * dir_x; x += dir_x)
 	{
 		int err = (y2 * (x - x1) - y1 * (x - x2)) - y_put * (x2 - x1);
-		printf("x = %d, err = %d\n", x, err);
-		if (ABS(err) >= ABS(x2 - x1))
+		if (ABS(err) > ABS(x2 - x1))
+		{
+			if (AA)
+			{
+				if (y_x_flip)
+					my_mlx_pixel_put(img_ptr, y_put, x, 0x00351300);
+				else
+					my_mlx_pixel_put(img_ptr, x, y_put, 0x00351300);
+			}
 			y_put += dir_y;
-		my_mlx_pixel_put(img_ptr, x, y_put, 0x00FF5C00);
-	}
-
-	int x_put = x1;
-	for (int y = y1; y * dir_y <= y2 * dir_y; y += dir_y)
-	{
-		int err = (x2 * (y - y1) - x1 * (y - y2)) - x_put * (y2 - y1);
-		if (ABS(err) >= ABS(y2 - y1))
-			x_put += dir_x;
-		my_mlx_pixel_put(img_ptr, x_put, y, 0x00FF5C00);
+		}
+		if (y_x_flip)
+			my_mlx_pixel_put(img_ptr, y_put, x, 0x00FF5C00);
+		else
+			my_mlx_pixel_put(img_ptr, x, y_put, 0x00FF5C00);
 	}
 }
 
@@ -69,8 +90,8 @@ int main(void)
 
 	// -- Cool Stuff
 	int i = 0;
-	int cycles = 32;
-	int m = 200;
+	int cycles = 10000;
+	int m = 300;
 	while (i < cycles)
 	{
 		draw_line(&img_data, WINDOW_WIDTH/2, WINDOW_HEIGHT/2, WINDOW_WIDTH/2 + m * cos(i * 2 * PI/cycles), WINDOW_HEIGHT/2 + m * sin(i * 2 * PI/cycles));
