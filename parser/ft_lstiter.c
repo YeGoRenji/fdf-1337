6c104e13fc6cb22253e9b88590fb50e6cb3bfacf
YeGoTaChi <yego.designs@gmail.com>
2023-06-12 20:52:02 +0100

    Feat: forgot to add parser

---
+++ parser/ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 00:12:51 by ylyoussf          #+#    #+#             */
+/*   Updated: 2023/06/12 14:34:17 by ylyoussf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/parser.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
---
+++ parser/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 22:39:36 by ylyoussf          #+#    #+#             */
+/*   Updated: 2023/06/12 14:34:35 by ylyoussf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/parser.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
